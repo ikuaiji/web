844d16095fc6dc4c7cf5fa076f64d1c4306fc946
Athurg Feng <hi@atfeng.com>
2021-04-09 11:04:02 +0800

    用Elementplus改造默认布局和导航栏

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <el-container>
+    <el-header>
+      <el-menu :default-active="activeNav" mode="horizontal" :router="true">
+        <el-menu-item index="Home" :route="{name: 'Home'}">Home</el-menu-item>
+        <el-menu-item index="About" :route="{name: 'About'}">About</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main><router-view/></el-main>
+  </el-container>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default{
+  computed: {
+    activeNav() {
+      return this.$route.name
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
