c88cf26e00751938a62f418eac054fa3e3315f2f
Athurg Feng <hi@atfeng.com>
2021-04-09 20:54:12 +0800

    支持账目的编辑功能（仅前端，暂不能保存到服务器）

--- src/views/Bills.vue
+++ src/views/Bills.vue
@@ -1,8 +1,91 @@
 <template>
   <template v-for="iter of bills_by_day" :key="iter.first" >
     <el-divider content-position="left">{{iter[0]}}</el-divider>
+    <el-table :data="iter[1]" row-key="ID">
+      <el-table-column type="expand" look="展开">
+        <template #default="props">
+          <el-form inline size="mini">
+            <el-row>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-select v-model="props.row.CategoryId">
+                    <template #prefix>科目</template>
+                    <el-option label="未设置" :value="0"></el-option>
+                    <el-option v-for="(categoryName,categoryId) of id_names.categories" :key="categoryId" :label="categoryName" :value="parseInt(categoryId)"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-select v-model="props.row.AccountId">
+                    <template #prefix>账户</template>
+                    <el-option label="未设置" :value="0"></el-option>
+                    <el-option v-for="(accountName,accountId) of id_names.accounts" :key="accountId" :label="accountName" :value="parseInt(accountId)"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-select v-model="props.row.MemberId">
+                    <template #prefix>成员</template>
+                    <el-option label="未设置" :value="0"></el-option>
+                    <el-option v-for="(memberName,memberId) of id_names.members" :key="memberId" :label="memberName" :value="parseInt(memberId)"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-input :controls="false" :precision="2" type="number" v-model="props.row.Amount">
+                    <template #prefix>金额</template>
+                  </el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
 
-    <el-table :data="iter[1]">
+            <el-row>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-select v-model="props.row.ProjectId">
+                    <template #prefix>项目</template>
+                    <el-option label="未设置" :value="0"></el-option>
+                    <el-option v-for="(projectName,projectId) of id_names.projects" :key="projectId" :label="projectName" :value="parseInt(projectId)"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-select v-model="props.row.StoreId">
+                    <template #prefix>商户</template>
+                    <el-option label="未设置" :value="0"></el-option>
+                    <el-option v-for="(storeName,storeId) of id_names.stores" :key="storeId" :label="storeName" :value="parseInt(storeId)"></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :span="6">
+                <el-form-item>
+                  <el-date-picker size="mini" v-model="props.row.BillAt" type="datetime"></el-date-picker>
+                </el-form-item>
+              </el-col>
+            </el-row>
+
+            <el-row>
+              <el-col :span="12">
+                  <el-input size="mini" v-model="props.row.Note" clearable>
+                    <template #prefix>备注</template>
+                  </el-input>
+              </el-col>
+              <el-col :span="5" :offset="1">
+                <el-form-item>
+                  <el-button type="primary" icon="el-icon-upload" @click="saveBill(props.row)">保存</el-button>
+                  <el-button type="danger" icon="el-icon-delete" @click="removeBill(props.row)">删除</el-button>
+                </el-form-item>
+              </el-col>
+            </el-row>
+
+          </el-form>
+        </template>
+      </el-table-column>
+
       <el-table-column label="科目" width="120" align="center">
         <template #default="scope">{{ idToName("categories", scope.row.CategoryId) }}</template>
       </el-table-column>
@@ -17,8 +100,13 @@
       </el-table-column>
 
       <el-table-column label="金额" width="100" prop="Amount" align="right"></el-table-column>
+
       <el-table-column label="账号" width="120" align="center"><template #default="scope">{{ idToName("accounts", scope.row.AccountId) }}</template></el-table-column>
+
+      <el-table-column label="成员" width="120" align="center"><template #default="scope">{{ idToName("members", scope.row.MemberId) }}</template></el-table-column>
+
       <el-table-column label="备注" prop="Note"></el-table-column>
+
     </el-table>
   </template>
 </template>
@@ -34,8 +122,16 @@ export default{
     }
   },
   methods:{
-    idToName: function(type,id) {
+    idToName(type,id) {
       return this.id_names[type][id]
+    },
+    saveBill(bill) {
+      //TODO
+      console.log(bill)
+    },
+    removeBill(bill) {
+      //TODO
+      console.log(bill)
     },
   },
   mounted () {
