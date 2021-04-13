e9324532262c34879aa93248c67ab68929ae7c85
Athurg Feng <hi@atfeng.com>
2021-04-13 20:44:27 +0800

    修复隐藏账户导致账户卡片间隔异常的问题

--- src/views/Accounts.vue
+++ src/views/Accounts.vue
@@ -2,56 +2,46 @@
   <el-tabs tab-position="left">
     <el-tab-pane label="银行账户">
       <el-space wrap size="large">
-        <template v-for="iter of accounts.bank" :key="iter.ID" >
-          <el-card v-if="!iter.Hide">
-            <template #header>{{iter.Name}}</template>
+        <el-card v-for="iter of accounts.bank" :key="iter.ID">
+          <template #header>{{iter.Name}}</template>
 
-            <h1><span style="color:gray;font-size:10px;">CNY:</span>{{Math.round(account_balances[iter.ID] * 100)/100}}</h1>
-            <el-button class="button" type="text" icon="el-icon-edit">编辑</el-button>
-            <el-button class="button" type="text" icon="el-icon-delete">删除</el-button>
-            <el-button class="button" type="text" icon="el-icon-view">隐藏</el-button>
-            <el-button class="button" type="text" icon="el-icon-document">对账</el-button>
-          </el-card>
-        </template>
+          <h1><span style="color:gray;font-size:10px;">CNY:</span>{{get_balance(iter.ID)}}</h1>
+          <el-button class="button" type="text" icon="el-icon-edit">编辑</el-button>
+          <el-button class="button" type="text" icon="el-icon-delete">删除</el-button>
+          <el-button class="button" type="text" icon="el-icon-view">隐藏</el-button>
+          <el-button class="button" type="text" icon="el-icon-document">对账</el-button>
+        </el-card>
       </el-space>
     </el-tab-pane>
 
     <el-tab-pane label="虚拟账户">
       <el-space wrap size="large">
-        <template v-for="iter of accounts.virtual" :key="iter.ID" >
-          <el-card v-if="!iter.Hide">
-            <template #header>{{iter.Name}}</template>
+        <el-card v-for="iter of accounts.virtual" :key="iter.ID" >
+          <template #header>{{iter.Name}}</template>
 
-            <h1><span style="color:gray;font-size:10px;">CNY:</span>{{Math.round(account_balances[iter.ID] * 100)/100}}</h1>
-            <el-button class="button" type="text" icon="el-icon-edit">编辑</el-button>
-            <el-button class="button" type="text" icon="el-icon-delete">删除</el-button>
-            <el-button class="button" type="text" icon="el-icon-view">隐藏</el-button>
-            <el-button class="button" type="text" icon="el-icon-document">对账</el-button>
-          </el-card>
-        </template>
+          <h1><span style="color:gray;font-size:10px;">CNY:</span>{{get_balance(iter.ID)}}</h1>
+          <el-button class="button" type="text" icon="el-icon-edit">编辑</el-button>
+          <el-button class="button" type="text" icon="el-icon-delete">删除</el-button>
+          <el-button class="button" type="text" icon="el-icon-view">隐藏</el-button>
+          <el-button class="button" type="text" icon="el-icon-document">对账</el-button>
+        </el-card>
       </el-space>
     </el-tab-pane>
 
     <el-tab-pane label="借贷账户">
       <el-space wrap size="large">
-        <template v-for="iter of accounts.debt" :key="iter.ID" >
-          <el-card v-if="!iter.Hide">
-            <template #header>{{iter.Name}}</template>
+        <el-card v-for="iter of accounts.debt" :key="iter.ID" >
+          <template #header>{{iter.Name}}</template>
 
-            <h1><span style="color:gray;font-size:10px;">CNY:</span>{{Math.round(account_balances[iter.ID] * 100)/100}}</h1>
-            <el-button class="button" type="text" icon="el-icon-edit">编辑</el-button>
-            <el-button class="button" type="text" icon="el-icon-delete">删除</el-button>
-            <el-button class="button" type="text" icon="el-icon-view">隐藏</el-button>
-            <el-button class="button" type="text" icon="el-icon-document">对账</el-button>
-          </el-card>
-        </template>
+          <h1><span style="color:gray;font-size:10px;">CNY:</span>{{get_balance(iter.ID)}}</h1>
+          <el-button class="button" type="text" icon="el-icon-edit">编辑</el-button>
+          <el-button class="button" type="text" icon="el-icon-delete">删除</el-button>
+          <el-button class="button" type="text" icon="el-icon-view">隐藏</el-button>
+          <el-button class="button" type="text" icon="el-icon-document">对账</el-button>
+        </el-card>
       </el-space>
     </el-tab-pane>
   </el-tabs>
-  
-
-
-
 </template>
 
 <script>
@@ -69,6 +59,9 @@ export default{
     }
   },
   methods:{
+    get_balance(id) {
+      return Math.round(this.account_balances[id] * 100)/100
+    },
     saveBill(bill) {
       //TODO
       console.log(bill)
@@ -76,16 +69,30 @@ export default{
   },
   mounted () {
     let that=this
+
+    axios.get("/account_balances").then(function (response) {
+      if (response.data.code!=0) {
+        //todo:提示错误信息
+        return
+      }
+
+      that.account_balances = response.data.data
+    })
+
     axios.get("/accounts").then(function (response) {
       if (response.data.code!=0) {
         //todo:提示错误信息
         return
       }
 
+      //分类账户，并过滤掉隐藏账户
       let debt=[]
       let bank=[]
       let virtual=[]
       for (const account of response.data.data) {
+        if (account.Hide) {
+          continue;
+        }
         switch(account.Type) {
           case "debt": debt.push(account); break;
           case "bank": bank.push(account); break;
@@ -96,14 +103,6 @@ export default{
       that.accounts.debt = debt
       that.accounts.bank = bank
       that.accounts.virtual = virtual
-    })
-    axios.get("/account_balances").then(function (response) {
-      if (response.data.code!=0) {
-        //todo:提示错误信息
-        return
-      }
-
-      that.account_balances = response.data.data
     })
   }
 }
