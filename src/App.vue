1e0fb4041d8232c138acae8ad3c022224876a1fd
Athurg Feng <hi@atfeng.com>
2021-04-13 20:54:15 +0800

    移除没用的Vue Demo代码

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,9 @@
   <el-container>
     <el-header>
       <el-menu :default-active="activeNav" mode="horizontal" :router="true">
-        <el-menu-item index="Home" :route="{name: 'Home'}">Home</el-menu-item>
-        <el-menu-item index="Bills" :route="{name: 'Bills'}">Bills</el-menu-item>
-        <el-menu-item index="Accounts" :route="{name: 'Accounts'}">Accounts</el-menu-item>
-        <el-menu-item index="About" :route="{name: 'About'}">About</el-menu-item>
+        <el-menu-item index="Home" :route="{name: 'Home'}">概览</el-menu-item>
+        <el-menu-item index="Bills" :route="{name: 'Bills'}">记一笔</el-menu-item>
+        <el-menu-item index="Accounts" :route="{name: 'Accounts'}">账户</el-menu-item>
       </el-menu>
     </el-header>
     <el-main><router-view/></el-main>
